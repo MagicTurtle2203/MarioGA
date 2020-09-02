4fe25615ad789cae38b8870051135e1fd8495e8c
Rafferty Chen <raffertychen@gmail.com>
2020-09-02 04:16:02 -0700

    Fix negative probabilities

--- src/genetic_nn/population.py
+++ src/genetic_nn/population.py
@@ -17,7 +17,7 @@ class Population:
         for _ in range(POPULATION):
             self.genomes.append(Genome())
 
-    def evaluate_population(self, eval_func: Callable[[Genome], float]) -> np.ndarray[np.float64]:
+    def evaluate_population(self, eval_func: Callable[[Genome], float]) -> np.ndarray:
         self.fitnesses = np.zeros((POPULATION,))
 
         for idx, genome in enumerate(self.genomes):
@@ -26,18 +26,18 @@ class Population:
         return self.fitnesses
 
     def breed_next_generation(self) -> None:
-        probabilities: np.ndarray[np.float64] = self.fitnesses / self.fitnesses.sum()
+        probabilities: np.ndarray[np.float64] = np.maximum(self.fitnesses, 1) / np.maximum(self.fitnesses, 1).sum()
         new_generation: List[Genome] = []
 
-        for _ in range(POPULATION // 2):
+        for _ in range(self.rng.integers((POPULATION // 2) - 3, POPULATION // 2)):
             if self.rng.random() < 0.05:
                 new_generation.extend((Genome(), Genome()))
             else:
                 parent1, parent2 = self.rng.choice(self.genomes, size=2, replace=False, p=probabilities)
                 new_generation.extend(Genome.crossover(parent1, parent2))
 
-        if len(new_generation) < POPULATION:
-            new_generation.append(self.rng.choice(self.genomes, size=1, p=probabilities))
+        while len(new_generation) < POPULATION:
+            new_generation.append(self.rng.choice(self.genomes, size=1, p=probabilities)[0])
 
         for genome in new_generation:
             genome.mutate()
