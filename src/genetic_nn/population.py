43091ae6ce7b1c9a5a72e8b644895f4828317f8a
Rafferty Chen <raffertychen@gmail.com>
2020-09-02 00:56:02 -0700

    Implement Population class

---
+++ src/genetic_nn/population.py
@@ -0,0 +1,45 @@
+from typing import Callable, List
+
+import numpy as np
+
+from genetic_nn.genome import Genome
+
+NUM_INPUTS = 960
+NUM_OUTPUTS = 12
+HIDDEN_LAYERS = (32, 16)
+POPULATION = 20
+
+
+class Population:
+    def __init__(self) -> None:
+        self.genomes: List[Genome] = []
+        self.fitnesses: np.ndarray[float] = np.zeros((POPULATION,))
+
+        self.rng = np.random.default_rng()
+
+        for _ in range(POPULATION):
+            self.genomes.append(Genome(NUM_INPUTS, NUM_OUTPUTS, HIDDEN_LAYERS))
+
+    def evaluate_population(self, eval_func: Callable[[Genome], float]) -> float:
+        self.fitnesses.clear()
+
+        for idx, genome in enumerate(self.genomes):
+            self.fitnesses[idx] = eval_func(genome)
+
+        return max(self.fitnesses)
+
+    def breed_next_generation(self) -> None:
+        probabilities: np.ndarray[float] = self.fitnesses / self.fitnesses.sum()
+        new_generation: List[Genome] = []
+
+        for _ in range(POPULATION // 2):
+            parent1, parent2 = self.rng.choice(self.genomes, size=2, replace=False, p=probabilities)
+            new_generation.extend(Genome.crossover(parent1, parent2))
+
+        if len(new_generation) < POPULATION:
+            new_generation.append(self.rng.choice(self.genomes, size=1, p=probabilities))
+
+        for genome in new_generation:
+            genome.mutate()
+
+        self.genomes = new_generation
